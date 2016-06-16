2ba954192b72ed3e5e6e953aebbd705e01e4ff0e
Furqan Ahmed <ahmedfurqan88@gmail.com>
2016-06-16 15:59:50 +0300

    button changes

--- controller/buttons.php
+++ controller/buttons.php
@@ -5,22 +5,16 @@ $result = $conn->query($sql);
 $buttonDiv ="<div>";
 if($result->rowCount() > 0){
 	foreach($result as $row){
-		$buttonDiv.= '<button id="voteBtn" style="margin-left:15px" disabled class="btn btn-large btn-';
+		$buttonDiv.= '<button id="voteBtn" style="margin-left:15px" disabled class="btn btn-large">';
+		
 		if($row["id"] == "1") {
-			$buttonDiv.= 'danger"';
+			$buttonDiv.= '<img src="../pics/bad.png" onclick="controller.sendButtonPress(1)"/>';
 		} elseif($row["id"] == "2") {
-			$buttonDiv.= 'warning"';
+			$buttonDiv.= '<img src="../pics/neutral.png" onclick="controller.sendButtonPress(2)"/>';
 		} elseif($row["id"] == "3") {
-			$buttonDiv.= 'primary"';
-		} elseif($row["id"] == "4") {
-			$buttonDiv.= 'info"';
-		} elseif($row["id"] == "5") {
-			$buttonDiv.= 'success"';
+			$buttonDiv.= '<img src="../pics/good.png" onclick="controller.sendButtonPress(3)"/>';
 		}
-		$buttonDiv.=' onclick="controller.sendButtonPress(';
-		$buttonDiv.=$row["id"];
-		$buttonDiv.=');">';
-		$buttonDiv.= $row["name"];
+		
 		$buttonDiv.= '</button>';
 	}
 } else {
