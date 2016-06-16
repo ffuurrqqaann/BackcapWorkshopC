2a3298bfacf0e4a8ec320c18c2803b4776a4d51d
Pedro Coutinho <pedro.miguel.coutinho@gmail.com>
2016-06-16 12:30:22 +0100

    Reorganizing stuff

--- Deviceid.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '51Degrees/example/ExampleMaster.php';
-require_once '51Degrees/core/51Degrees.php';
-
-$ua = $_SERVER['HTTP_USER_AGENT'];
-$properties = fiftyone_degrees_get_device_data($ua);
-
-$mobileDeviceId = $properties['DeviceId'];
