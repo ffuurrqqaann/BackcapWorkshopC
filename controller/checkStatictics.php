76de74165c25ce2bca014c9eb2dae4e2345ea5ed
Furqan Ahmed <ahmedfurqan88@gmail.com>
2016-06-18 13:19:02 +0300

    checkstats

---
+++ controller/checkStatictics.php
@@ -0,0 +1,22 @@
+<?php
+require_once ("../utils/database.php");
+$question 	= $_GET['question_id'];
+
+try {
+
+	$sql= "SELECT count(id) AS bad,
+(SELECT COUNT(id) AS nVote FROM answers WHERE question_id=".$question."AND option_id=2) AS neutral,
+(SELECT COUNT(id) AS totalVote FROM answers WHERE question_id=".$question." AND option_id=3) AS good,
+(SELECT COUNT(id) AS totalVote FROM answers WHERE question_id=".$question.") AS totalVote
+FROM answers WHERE question_id=1 AND option_id=1";
+
+	$statement = $conn->prepare($sql);
+	$statement->execute();
+	$row = $statement->fetch();
+	
+	var_dump($row);
+	
+} catch(PDOException $e) {
+ 	echo "Error: " . $e->getMessage();
+}
+?>
